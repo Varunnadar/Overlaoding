4f47a2138e08bd0d507d613e604d291aff1e6da3
Varunnadar <nadarvarunlingam13@gmail.com>
2024-03-13 00:51:00 +0530

    new Overlaoding

--- Overloading/src/com/pack2/Overloadingg.java
+++ Overloading/src/com/pack2/Overloadingg.java
@@ -1,9 +1,18 @@
 package com.pack2;
 
+class A{
+	void meth1(int a ) {
+		System.out.println(a);
+	}
+	void meth1(String a) {
+		System.out.println(a);
+	}
+}
+	
 public class Overloadingg {
-	private void syso() {
-		// TODO Auto-generated method stub
+	
+		
 
 	}
 
-}
+
